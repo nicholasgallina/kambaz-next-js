78170d4a89815dc135f8d11c935a0f25ab37f63d
Nicholas Gallina <nicholasgallina@Aurora.local>
2025-09-19 17:28:56 -0400

    added link to github in Lab1

--- src/app/Labs/Lab1/page.tsx
+++ src/app/Labs/Lab1/page.tsx
@@ -3,6 +3,7 @@ export default function Lab1() {
     <div id="wd-lab1">
       <h2>Lab 1</h2>
       <h1>Nicholas Gallina, 11597</h1>
+      <a href="https://github.com/nicholasgallina/kambaz-next-js">GitHub</a>
     </div>
   );
 }
